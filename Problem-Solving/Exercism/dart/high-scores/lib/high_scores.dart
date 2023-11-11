ded0cda524fccd1fe4bffe60f912147cb4230fcf
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-12 01:08:05 +0200

    high scores problem solved

--- Problem-Solving/Exercism/dart/high-scores/lib/high_scores.dart
+++ Problem-Solving/Exercism/dart/high-scores/lib/high_scores.dart
@@ -1,3 +1,31 @@
 class HighScores {
-  // Put your code here
+  List<int> scores;
+
+  HighScores(this.scores);
+
+  int latest() {
+    return scores.last;
+  }
+
+  int personalBest() {
+    return scores.fold(
+        scores[0],
+        (previousValue, element) =>
+            (previousValue > element) ? previousValue : element);
+  }
+
+  List<int> personalTopThree() {
+    List<int> copyOfData = [...scores];
+    List<int> topThreeResult = [];
+
+    for (int i = 0; i < 3; i++) {
+      topThreeResult.add(copyOfData.fold(
+          copyOfData[0],
+          (previousValue, element) =>
+              (previousValue > element) ? previousValue : element));
+
+      copyOfData.remove(topThreeResult[i]);
+    }
+    return topThreeResult;
+  }
 }
