eae75e24f5210508462c26d95e48f4696dc05ef7
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-12 15:09:26 +0200

    grains problem solved

--- Problem-Solving/Exercism/dart/grains/lib/grains.dart
+++ Problem-Solving/Exercism/dart/grains/lib/grains.dart
@@ -1,9 +1,24 @@
+import 'dart:math';
+
 BigInt square(final int n) {
-  // Replace the throw call and put your code here
-  throw UnimplementedError();
+  if (n < 1 || n > 64) {
+    throw ArgumentError('square must be between 1 and 64');
+  } else {
+    BigInt sum = BigInt.from(0);
+
+    for (int i = 1; i <= n; i++) {
+      sum = BigInt.from(pow(2, i - 1).toInt());
+    }
+    return sum;
+  }
 }
 
 BigInt total() {
-  // Replace the throw call and put your code here
-  throw UnimplementedError();
+  BigInt total = BigInt.from(0);
+  for (int i = 1; i <= 64; i++) {
+    print(BigInt.from(pow(2, i - 1).toInt()));
+    total += BigInt.from(pow(2, i - 1).toInt());
+  }
+  return total;
 }
+
