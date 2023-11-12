c7a15b7ae49c2257b9b85a87011fa4b193e80d8f
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-12 17:28:58 +0200

    word-count problem solved

--- Problem-Solving/Exercism/dart/word-count/lib/word_count.dart
+++ Problem-Solving/Exercism/dart/word-count/lib/word_count.dart
@@ -1,3 +1,21 @@
 class WordCount {
   // Put your code here
+
+  Map<String, int> countWords(String subtitle) {
+    Map<String, int> wordCounts = {};
+
+    List<String> words =
+        subtitle.toLowerCase().split(RegExp(r"[^a-zA-Z0-9']+"));
+
+    for (String word in words) {
+      // Remove leading and trailing apostrophes
+      word = word.replaceAll(RegExp(r"^'+|'+$"), '');
+
+      // Update word count in the map
+      wordCounts[word] = (wordCounts[word] ?? 0) + 1;
+    }
+
+    return wordCounts;
+  }
 }
+
