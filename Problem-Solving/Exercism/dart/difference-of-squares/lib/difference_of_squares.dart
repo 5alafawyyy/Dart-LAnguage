5b6fbdd87ef71fa16f8c8d2faa4877818b253453
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-12 02:00:08 +0200

    difference-of-squares problem solved

--- Problem-Solving/Exercism/dart/difference-of-squares/lib/difference_of_squares.dart
+++ Problem-Solving/Exercism/dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,3 +1,41 @@
 class DifferenceOfSquares {
-  // Put your code here
+  int squareOfSum(int n) {
+    int _sum = 0;
+    for (int i = 1; i <= n; i++) {
+      _sum += i;
+    }
+    return (_sum * _sum);
+  }
+
+  int sumOfSquares(int n) {
+    int _sumOfSquaresResult = 0;
+    for (int i = 1; i <= n; i++) {
+      _sumOfSquaresResult += (i * i);
+    }
+    return _sumOfSquaresResult;
+  }
+
+  int differenceOfSquares(int n) {
+    return squareOfSum(n) - sumOfSquares(n);
+  }
 }
+
+/* ============== Another Sol ==================*/
+/**
+ class DifferenceOfSquares {
+  int squareOfSum(int n) {
+    final sum = Iterable.generate(n).fold(0, (acc, i) => (acc + i + 1).toInt());
+    return sum * sum;
+  }
+
+  int sumOfSquares(int n) {
+    return Iterable.generate(n)
+        .map((i) => (i + 1) * (i + 1))
+        .fold(0, (acc, val) => (acc + val).toInt());
+  }
+
+  int differenceOfSquares(int n) {
+    return squareOfSum(n) - sumOfSquares(n);
+  }
+}
+ */
