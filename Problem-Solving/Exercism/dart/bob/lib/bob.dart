32c869eff16a7fcb8f2f96e747e39503d8b6d8aa
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-21 13:21:25 +0200

    bob problem solved

--- Problem-Solving/Exercism/dart/bob/lib/bob.dart
+++ Problem-Solving/Exercism/dart/bob/lib/bob.dart
@@ -1,3 +1,28 @@
 class Bob {
   // Put your code here
+  late String result;
+  String response(String phrase) {
+    switch (phrase) {
+      case r"\?":
+        result = "Sure";
+
+        break;
+      case r"[A-Z]":
+        result = "Whoa, chill out!";
+        break;
+      case r"[A-Z\?]":
+        result = "Calm down, I know what I'm doing!";
+        break;
+      case r'^\s*$':
+        result = "Fine. Be that way!";
+        break;
+      default:
+        result = "Whatever.";
+    }
+    return result;
+  }
+}
+
+void main() {
+  print(Bob().response('Tom-ay-to, tom-aaaah-to.'));
 }
