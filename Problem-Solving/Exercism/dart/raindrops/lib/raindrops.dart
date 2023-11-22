4f2e1861d7a862345c9cd34668ef5b8caf3108a8
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-22 12:51:18 +0200

    raindrops problem solved

--- Problem-Solving/Exercism/dart/raindrops/lib/raindrops.dart
+++ Problem-Solving/Exercism/dart/raindrops/lib/raindrops.dart
@@ -1,3 +1,18 @@
 class Raindrops {
-  // Put your code here
+  String convert(int i) {
+    String result = '';
+    if (i % 3 == 0) {
+      result += 'Pling';
+    }
+    if (i % 5 == 0) {
+      result += 'Plang';
+    }
+    if (i % 7 == 0) {
+      result += 'Plong';
+    } else {
+      result = '$i';
+    }
+
+    return result.isNotEmpty ? result : i.toString();
+  }
 }
