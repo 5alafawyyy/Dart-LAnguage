5b4044ef7fe3fcd158e393309b31404336a36ad6
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-22 11:41:07 +0200

    hamming problem solved

--- Problem-Solving/Exercism/dart/hamming/lib/hamming.dart
+++ Problem-Solving/Exercism/dart/hamming/lib/hamming.dart
@@ -1,3 +1,18 @@
 class Hamming {
-  // Put your code here
+  distance(String s, String t) {
+    List<String> s1 = s.split("").toList();
+    List<String> s2 = t.split("").toList();
+    int distanceCount = 0;
+
+    if (s1.length != s2.length) {
+      throw ('strands must be of equal length');
+    } else {
+      for (int i = 0; i < s1.length; i++) {
+        if (s1[i] != s2[i]) {
+          distanceCount++;
+        }
+      }
+      return distanceCount;
+    }
+  }
 }
