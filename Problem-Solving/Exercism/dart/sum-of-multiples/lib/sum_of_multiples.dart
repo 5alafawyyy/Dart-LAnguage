d8c9d2d4637e55c940cb6f51e91e433a010b91c5
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-11-22 13:13:09 +0200

    sum of multiples problem solved

--- Problem-Solving/Exercism/dart/sum-of-multiples/lib/sum_of_multiples.dart
+++ Problem-Solving/Exercism/dart/sum-of-multiples/lib/sum_of_multiples.dart
@@ -1,3 +1,13 @@
 class SumOfMultiples {
-  // Put your code here
+  int sum(List<int> multiples, int max) {
+    Set<int> multiplesSet = {};
+
+    for (int multiple in multiples) {
+      for (int i = 1; multiple * i < max; i++) {
+        multiplesSet.add(multiple * i);
+      }
+    }
+
+    return multiplesSet.fold(0, (acc, curr) => acc + curr);
+  }
 }
