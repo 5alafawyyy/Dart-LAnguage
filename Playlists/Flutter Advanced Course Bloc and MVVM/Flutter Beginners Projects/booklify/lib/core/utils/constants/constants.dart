9db675704bce915bb5bc33e6dbd813ba2ec0c090
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-12-19 15:53:29 +0200

    adding animation to splash view

--- Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/core/utils/constants/constants.dart
+++ Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/core/utils/constants/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static const Color mainColor  = Color(0xFF090614);
+  static const Color mainColor  = Color(0xFF08060c);
   static const Color greyColor = Color(0xFF616161);
   static const Color whiteColor = Color(0xFFFFFFFF);
 }
--- Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets/assets.dart';
 import '../../../../../home.dart';
+import 'sliding_text.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -12,30 +13,58 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> {
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
   @override
   void initState() {
-    super.initState();
-    // Simulate some initialization work
-    Timer(
-      const Duration(seconds: 3),
-      () {
-        // Navigate to the main screen after 3 seconds
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const HomeScreen()),
-        );
-      },
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
     );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+
+    // Simulate some initialization work
+    // Timer(
+    //   const Duration(seconds: 3),
+    //   () {
+    //     // Navigate to the main screen after 3 seconds
+    //     Navigator.pushReplacement(
+    //       context,
+    //       MaterialPageRoute(builder: (context) => const HomeScreen()),
+    //     );
+    //   },
+    // );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(
-        AssetsData.logo,
-        scale: 0.7,
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.asset(
+            AssetsData.logo,
+          ),
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
     );
   }
 }
