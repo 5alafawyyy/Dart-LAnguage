ce91a1eb2df4d2927feac770d8a6b463bd384809
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-12-19 19:12:39 +0200

    add custom appbar to home view

--- Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/core/utils/assets/assets.dart
+++ Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/core/utils/assets/assets.dart
@@ -1,3 +1,5 @@
 class AssetsData {
   static const String logo = 'assets/images/logo.png';
+  static const String logoText = 'assets/images/logo_text.png';
+  static const String searchIcon = 'assets/icons/search.png';
 }
