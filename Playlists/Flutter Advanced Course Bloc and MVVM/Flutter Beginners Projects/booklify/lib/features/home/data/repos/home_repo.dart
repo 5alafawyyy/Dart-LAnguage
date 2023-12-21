6aab8b8fe13782cfa82daaf7b8aeec8cbc6fc68c
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-12-21 16:52:32 +0200

    create home repo file & failure file

--- Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/features/home/data/repos/home_repo.dart
+++ Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/features/home/data/repos/home_repo.dart
@@ -1,3 +1,9 @@
+import 'package:booklify/features/home/data/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
