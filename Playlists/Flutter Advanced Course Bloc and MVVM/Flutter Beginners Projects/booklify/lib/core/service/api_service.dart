78cc61d37470e6166048f2936821016685affac6
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-12-21 19:05:34 +0200

    Implement fetch newest books method

--- Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/core/service/api_service.dart
+++ Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/core/service/api_service.dart
@@ -1,13 +1,13 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  ApiService(this.dio);
-  final Dio dio;
+  ApiService(this._dio);
+  final Dio _dio;
 
   final String _baseUrl = 'https://www.googleapis.com/books/v1/';
 
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    final response = await dio.get('$_baseUrl$endPoint');
+    final response = await _dio.get('$_baseUrl$endPoint');
     return response.data;
   }
 }
--- Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/features/home/data/repos/home_repo_impl.dart
+++ Playlists/Flutter Advanced Course Bloc and MVVM/Flutter Beginners Projects/booklify/lib/features/home/data/repos/home_repo_impl.dart
@@ -1,19 +1,32 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../../../../core/service/api_service.dart';
 import '../book_model/book_model.dart';
 import 'base_home_repo.dart';
 
-class HomeRepoImpl implements BaseHomeRepo{
+class HomeRepoImpl implements BaseHomeRepo {
+  ApiService apiService;
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
-  
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      final data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
     throw UnimplementedError();
   }
-  
 }
